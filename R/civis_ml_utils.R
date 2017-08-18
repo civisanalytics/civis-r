f0d4808eec3045e9c6a13d31b27197703f63fbbb
Patrick Miller <pmiller@civisanalytics.com>
2017-08-18 16:36:39 -0500

    [RCL-92] print method for civis_ml

---
+++ R/civis_ml_utils.R
@@ -0,0 +1,45 @@
+# print, summary, plot -----
+
+#' @export
+print.civis_ml_classifier <- function(x, ...) {
+  class_names <- c(x$model_info$data$class_names)
+  tab <- with(x$metrics$metrics, t(cbind(roc_auc, p_correct)))
+  colnames(tab) <- class_names
+  rownames(tab) <- c("AUC", "Prop Correct")
+  print.civis_ml(x, tab = tab, ...)
+}
+
+#' @export
+print.civis_ml_regressor <- function(x, ...) {
+  dv_names <- x$model_info$data$target_columns
+  tab <- with(x$metrics$metrics, t(cbind(mad, rmse, r_squared)))
+  colnames(tab) <- dv_names
+  rownames(tab) <- c("MAD", "RMSE", "R-squared")
+  print.civis_ml(x, tab, ...)
+}
+
+#' @export
+print.civis_ml <- function(x, tab, digits = 4, ...) {
+  wfl <- model_workflow(x)
+  run_id <- x$run$id
+  job_id <- x$job$id
+  cat("<CivisML ", wfl, ">\n", sep = "")
+  cat("Job id: ", job_id, " Run id: ", run_id, "\n", fill = TRUE)
+  print(signif(tab, digits = digits))
+  invisible(x)
+}
+
+#' @export
+print.predict_civis_ml <- function(x, ...) {
+  cat("<CivisML Prediction>\n", sep = "")
+}
+
+model_type <- function(job) {
+  is_regr <- stringr::str_detect(job$arguments$MODEL, "regressor")
+  type <- if(is_regr) "regressor" else "classifier"
+  type
+}
+
+model_workflow <- function(m) {
+  m$job$arguments$MODEL
+}
---
+++ tools/generate_civis_ml_test_data.R
@@ -0,0 +1,44 @@
+library(civis)
+library(future)
+
+cat("setting up models...", fill =T)
+plan("multisession", workers = 20)
+
+class_algo <- c("sparse_logistic", "gradient_boosting_classifier", "random_forest_classifier", "extra_trees_classifier")
+reg_algo <- paste0(c("sparse_linear", "sparse_ridge", "gradient_boosting", "random_forest", "extra_trees"), "_regressor")
+
+do_class <- function(algo) {
+  future(civis_ml(civis_table("datascience.iris", "redshift-general"),
+           dependent_variable = "type",
+           model_type = algo))
+}
+
+data("ChickWeight")
+tmp <- tempfile()
+write.csv(ChickWeight, file = tmp)
+id <- write_civis_file(tmp, name = "chicweight.csv")
+
+do_regr <- function(algo, id) {
+  future(civis_ml(civis_file(id), "weight", algo))
+}
+unlink(tmp)
+
+
+cat("submitting models...", fill = T)
+
+mclass_fut <- lapply(class_algo, do_class)
+mreg_fut <- lapply(reg_algo, do_regr, id = id)
+multi_output_fut <- future(civis_ml(civis_file(id), dependent_variable = c("weight", "Time"),
+                                model_type = "random_forest_regressor"))
+
+cat("waiting for models to complete...", fill = TRUE)
+mclass_list <- lapply(mclass_fut, value)
+cat("classification models completed", fill = TRUE)
+mreg_list <- lapply(mreg_fut, value)
+cat("regression models completed", fill = TRUE)
+multi_output <- value(multi_output_fut)
+cat("multi output completed", fill = TRUE)
+ms <- c(mclass_list, mreg_list, list(multi_output))
+
+cat("writing tests/testthat/data/civis_ml_models.rds", fill = T)
+saveRDS(ms, "../tests/testthat/data/civis_ml_models.rds")
