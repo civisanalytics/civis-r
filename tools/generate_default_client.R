2751e9d899c16de924e7f0c530cc1dd61ab2e617
Patrick Miller <pmiller@civisanalytics.com>
2017-11-20 14:24:02 -0600

    revert auto-gen

--- tools/generate_default_client.R
+++ tools/generate_default_client.R
@@ -1,24 +1,16 @@
-# To generate a new client, run as:
-# Rscript generate_default_client.R
+# To generate a new client, save the default civis api spec locally
+# and pass the filename as an arg to this script.  i.e.
+# Rscript generate_default_client.R default_api_spec.json
 
-library(civis)
+source("../R/client_base.R")
 source("../R/generate_client.R")
+source("../R/utils.R")
 
-cat("Running robot template script...", fill = TRUE)
-robot_script_id <- 9925
-job <- scripts_post_custom(robot_script_id)
-run <- scripts_post_custom_runs(job$id)
-res <- await(scripts_get_custom_runs, id = job$id, run_id = run$id)
-fileid <- scripts_list_custom_runs_outputs(id = job$id, run_id = run$id)[[1]]$objectId
-cat("Downloading robot user spec...", fill = TRUE)
-fn <- tempfile(fileext = ".json")
-download_civis(fileid, fn)
-api_spec <- jsonlite::fromJSON(fn, simplifyVector = FALSE)
+args <- commandArgs(trailingOnly = TRUE)
+api_spec <- jsonlite::fromJSON(args[1], simplifyVector=FALSE)
 
 # To pass R CMD CHECK, this has to be an 'R Data file' (rda) produced by save.
-cat("saving sysdata.rda...", fill = TRUE)
 save(api_spec, file = "../R/sysdata.rda")
-cat("generating default client...", fill = TRUE)
-client_str <- civis:::generate_client(api_spec)
-civis:::write_client(client_str, FILENAME = paste0("../", FILENAME))
+client_str <- generate_client(api_spec)
+write_client(client_str, FILENAME = paste0("../", FILENAME))
 devtools::document()
