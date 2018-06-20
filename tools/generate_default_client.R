8ef8b13306fbd02bfef815b8d1291dcdecca355b
Patrick Miller <pmiller@civisanalytics.com>
2018-06-20 08:31:17 -0500

    Update client

--- tools/generate_default_client.R
+++ tools/generate_default_client.R
@@ -1,16 +1,25 @@
 # To generate a new client, save the default civis api spec locally
 # and pass the filename as an arg to this script.  i.e.
-# Rscript generate_default_client.R default_api_spec.json
+# Rscript generate_default_client.R
 
-source("../R/client_base.R")
-source("../R/generate_client.R")
-source("../R/utils.R")
+library(civis)
 
-args <- commandArgs(trailingOnly = TRUE)
-api_spec <- jsonlite::fromJSON(args[1], simplifyVector=FALSE)
+job <- scripts_post_custom(from_template_id = 9925)
+run <- scripts_post_custom_runs(job$id)
+await(scripts_get_custom_runs, id = job$id, run_id = run$id)
+spec_id <- scripts_list_custom_runs_outputs(job$id, run$id)[[1]]$objectId
+fn <- tempfile(fileext = ".json")
+download_civis(spec_id, file = fn)
+
+stopifnot(Sys.getenv("R_CLIENT_DEV") == "")
+key <- Sys.getenv("CIVIS_API_KEY")
+Sys.setenv("CIVIS_API_KEY" = "")
+
+api_spec <- jsonlite::fromJSON(fn, simplifyVector=FALSE)
 
 # To pass R CMD CHECK, this has to be an 'R Data file' (rda) produced by save.
 save(api_spec, file = "../R/sysdata.rda")
-client_str <- generate_client(api_spec)
-write_client(client_str, FILENAME = paste0("../", FILENAME))
+client_str <- civis:::generate_client(api_spec)
+civis:::write_client(client_str, FILENAME = paste0("../", civis:::FILENAME))
 devtools::document()
+Sys.setenv("CIVIS_API_KEY" = key)
