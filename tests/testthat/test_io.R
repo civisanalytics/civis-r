7899eaea586f640f0d55822cc0de9e79dc22abc4
Patrick Miller <pmiller@civisanalytics.com>
2019-05-16 09:30:33 -0500

    R CMD check

--- tests/testthat/test_io.R
+++ tests/testthat/test_io.R
@@ -23,7 +23,7 @@ test_that("read_civis.sql reads csvs", {
     `civis::download_script_results` = mock_download_script_results,
     `civis::stop_for_status` = function(...) return(TRUE),
     expect_equal(mock_df, read_civis(x = "lazy", database = "jellyfish")),
-    expect_equal(mock_df, read_civis(dbplyr::sql("SELECT * FROM lazy"),
+    expect_equal(mock_df, read_civis(sql("SELECT * FROM lazy"),
                                      database = "jellyfish"))
   )
 })
@@ -35,7 +35,7 @@ test_that("read_civis.sql produces catchable error when query returns no rows", 
   with_mock(
     `civis::start_scripted_sql_job` = mock_sql_job,
     `civis::scripts_get_sql_runs` = function(...) no_results_resp,
-    try_err <- try(read_civis(dbplyr::sql("SELECT 0"), database = "arrgh"), silent = TRUE),
+    try_err <- try(read_civis(sql("SELECT 0"), database = "arrgh"), silent = TRUE),
     expect_true("empty_result_error" %in% class(attr(try_err, "condition")))
   )
 })
