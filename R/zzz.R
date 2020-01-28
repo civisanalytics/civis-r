55edc20db81310e081cbbdda142c1a01cf4c927f
Sahil Shah <sshah2@civisanalytics.com>
2020-01-28 16:16:02 -0600

    REF Modify to use current production version alias id

--- R/zzz.R
+++ R/zzz.R
@@ -7,23 +7,14 @@
 }
 
 get_train_template_id <- function() {
-  versions <- unique(CIVIS_ML_TEMPLATE_IDS$version)
-  latest <- max(versions)
-  id <- CIVIS_ML_TEMPLATE_IDS[CIVIS_ML_TEMPLATE_IDS$version == latest &
-                              CIVIS_ML_TEMPLATE_IDS$name == "train", "id"]
 
-  if (!inherits(try(scripts_list_custom(id), silent = TRUE), "try-error")) {
-    i <- 1
-    while (length(scripts_list_custom(id)) == 0 &&
-           i < length(versions)) {
-      # use the previous version; assume that only the latest version is in internal release
-      prev <- versions[length(versions) - i]
-      id <- CIVIS_ML_TEMPLATE_IDS[CIVIS_ML_TEMPLATE_IDS$version == prev &
-                                  CIVIS_ML_TEMPLATE_IDS$name == "train", "id"]
-      i <- i + 1
-    }
-  }
+  CIVIS_ML_TEMPLATE_IDS <- get_template_ids_all_versions()
+
+  id <- CIVIS_ML_TEMPLATE_IDS[CIVIS_ML_TEMPLATE_IDS$version == "prod" &
+                              CIVIS_ML_TEMPLATE_IDS$name == "training", "id"]
+
   return(id)
+
 }
 
 
