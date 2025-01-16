1203cdcba2e50943baab5d5dcbf2447733f0aaa7
Michael Heilman <mheilman@civisanalytics.com>
2025-01-16 13:43:34 -0600

    make sure testthat package is recent

--- tools/install-and-run-tests.R
+++ tools/install-and-run-tests.R
@@ -1,4 +1,8 @@
 # This script is for running tests in Docker, in Travis CI or locally.
-install.packages("devtools")
+install.packages(c(
+    "devtools",
+    # Make sure we have a recent version of testthat.
+    "testthat"
+))
 devtools::install_local(".", dependencies = TRUE)
 devtools::check()
