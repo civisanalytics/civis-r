c59ba814c50f4ad703c12c3da474bdaee3a49495
Michael Heilman <mheilman@civisanalytics.com>
2025-01-16 15:48:48 -0600

    Revert "make sure testthat package is recent"
    This reverts commit 1203cdcba2e50943baab5d5dcbf2447733f0aaa7.

--- tools/install-and-run-tests.R
+++ tools/install-and-run-tests.R
@@ -1,8 +1,4 @@
 # This script is for running tests in Docker, in Travis CI or locally.
-install.packages(c(
-    "devtools",
-    # Make sure we have a recent version of testthat.
-    "testthat"
-))
+install.packages("devtools")
 devtools::install_local(".", dependencies = TRUE)
 devtools::check()
